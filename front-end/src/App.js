e09cea00f20a3e435d59ec08a1feda046820c691
Eldiyar888 <jashtilekov888@gmail.com>
2022-10-16 19:16:54 +0600

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Content from './pages/Content/Content';
+
+function App() {
+  return (
+    <div className="App">
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/pages/Content/Content.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Content = () => {
+    const [error, setError] = useState(null);
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:1337/api/products')
+            .then(resp => {
+                setProduct(resp.data.data);
+                console.log(resp.data.data)
+            })
+            .catch(error => {
+                setError(error);
+            })
+    }, []);
+
+    if (error) {
+        return <div>An error occured: {error.message}</div>;
+    }
+
+    return (
+        <div className='wrapper'> 
+            <div className='item-wrapper'>
+                {
+                    product.map((item) => (
+                        <div className='item'> <h2>{item.attributes.name.toUpperCase()}</h2> <p> <b><i>Описание:</i></b> {item.attributes.description}</p> <p> <b><i>Цена:</i></b> {item.attributes.price}</p> </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Content;
